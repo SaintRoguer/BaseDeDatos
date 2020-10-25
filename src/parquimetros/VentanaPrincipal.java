958fa6ffa802eef8cea6b0fd702ec5118a4de633
IVChristos <anonymousvsworld@users.noreply.github.com>
2020-10-25 10:08:16 -0300

    Update VentanaPrincipal.java
    Ventana principal funcionando correctamente, pueden haber cambios para que se vea mejor.

--- src/parquimetros/VentanaPrincipal.java
+++ src/parquimetros/VentanaPrincipal.java
@@ -3,13 +3,22 @@ import java.awt.BorderLayout;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.beans.PropertyVetoException;
 
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JDesktopPane;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JSeparator;
+import javax.swing.LayoutStyle;
+import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
 
@@ -19,17 +28,22 @@ import javax.swing.WindowConstants;
 public class VentanaPrincipal extends javax.swing.JFrame 
 {
 
+   private VentanaInspector ventanaBarcos;
    private VentanaConsultas ventanaConsultas;
-   private VentanaInspector ventanaInspector;
+   private JButton btnCerrar;
+   private JLabel lblAcercaDe4;
+   private JLabel lblAcercaDe3;
+   private JLabel lblAcercaDe2;
+   private JLabel lblAcercaDe1;
+   private JDialog dlgAcercaDe;
 
    private JDesktopPane jDesktopPane1;
    private JMenuBar jMenuBar1;
    private JMenuItem mniSalir;
    private JSeparator jSeparator1;
-   private JMenuItem mniInspector;
-   private JMenu mnuEjemplos;
-   private JSeparator jSeparator1_1;
+   private JMenuItem mniBarcos;
    private JMenuItem mniConsultas;
+   private JMenu mnuEjemplos;
 
    /**
    * Auto-generated main method to display this JFrame
@@ -48,11 +62,14 @@ public class VentanaPrincipal extends javax.swing.JFrame
    public VentanaPrincipal() 
    {
       super();
-      setResizable(false);
 
       initGUI();
 
-          
+      this.ventanaBarcos = new VentanaInspector();
+      ventanaBarcos.setLocation(0, -12);
+      this.ventanaBarcos.setVisible(false);
+      this.jDesktopPane1.add(this.ventanaBarcos);
+      
       this.ventanaConsultas = new VentanaConsultas();
       this.ventanaConsultas.setVisible(false);
       this.jDesktopPane1.add(this.ventanaConsultas);
@@ -72,13 +89,88 @@ public class VentanaPrincipal extends javax.swing.JFrame
       
       try {
          {
-            this.setTitle("Proyecto base de datos - Parquimetros");
+            this.setTitle("Java y MySQL");
             this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
          }
          {
             jDesktopPane1 = new JDesktopPane();
             getContentPane().add(jDesktopPane1, BorderLayout.CENTER);
             jDesktopPane1.setPreferredSize(new java.awt.Dimension(800, 600));
+         }
+         {
+            dlgAcercaDe = new JDialog(this);
+            GroupLayout dlgAcercaDeLayout = new GroupLayout((JComponent)dlgAcercaDe.getContentPane());
+            dlgAcercaDe.getContentPane().setLayout(dlgAcercaDeLayout);
+            dlgAcercaDe.setTitle("Acerca de...");
+            dlgAcercaDe.setMinimumSize(new java.awt.Dimension(394, 202));
+            dlgAcercaDe.setModalityType(java.awt.Dialog.ModalityType.APPLICATION_MODAL);
+            dlgAcercaDe.setUndecorated(true);
+            {
+               lblAcercaDe1 = new JLabel();
+               lblAcercaDe1.setFont(new java.awt.Font("Dialog", 1, 14));
+               lblAcercaDe1.setText("Ejemplo de conexión a MySQL mediante JDBC");
+               lblAcercaDe1.setHorizontalAlignment(SwingConstants.CENTER);
+            }
+            {
+               btnCerrar = new JButton();
+               btnCerrar.setText("Cerrar");
+               btnCerrar.addActionListener(new ActionListener() {
+                  public void actionPerformed(ActionEvent evt) {
+                     btnCerrarActionPerformed(evt);
+                  }
+               });
+            }
+            {
+               lblAcercaDe2 = new JLabel();
+               lblAcercaDe2.setText("Versión: 0.2");
+               lblAcercaDe2.setHorizontalAlignment(SwingConstants.CENTER);
+            }
+            {
+               lblAcercaDe3 = new JLabel();
+               lblAcercaDe3.setText("<html><center>Bases de Datos<br/>Elementos de Bases de Datos<br/>Teoría y Diseño de Bases de Datos<br/>");
+               lblAcercaDe3.setHorizontalTextPosition(SwingConstants.LEADING);
+               lblAcercaDe3.setHorizontalAlignment(SwingConstants.CENTER);
+            }
+            {
+               lblAcercaDe4 = new JLabel();
+               lblAcercaDe4.setText("<html><center>Dpto. de Ciencias e Ingeniería de la Computación<br/>Universidad Nacional del Sur");
+               lblAcercaDe4.setHorizontalAlignment(SwingConstants.CENTER);
+            }
+            dlgAcercaDeLayout.setHorizontalGroup(dlgAcercaDeLayout.createSequentialGroup()
+               .addContainerGap(62, 62)
+               .addGroup(dlgAcercaDeLayout.createParallelGroup()
+                   .addGroup(dlgAcercaDeLayout.createSequentialGroup()
+                       .addComponent(lblAcercaDe3, GroupLayout.PREFERRED_SIZE, 367, GroupLayout.PREFERRED_SIZE)
+                       .addGap(0, 0, Short.MAX_VALUE))
+                   .addGroup(dlgAcercaDeLayout.createSequentialGroup()
+                       .addComponent(lblAcercaDe4, GroupLayout.PREFERRED_SIZE, 367, GroupLayout.PREFERRED_SIZE)
+                       .addGap(0, 0, Short.MAX_VALUE))
+                   .addGroup(dlgAcercaDeLayout.createSequentialGroup()
+                       .addComponent(lblAcercaDe1, GroupLayout.PREFERRED_SIZE, 367, GroupLayout.PREFERRED_SIZE)
+                       .addGap(0, 0, Short.MAX_VALUE))
+                   .addGroup(dlgAcercaDeLayout.createSequentialGroup()
+                       .addGap(127)
+                       .addGroup(dlgAcercaDeLayout.createParallelGroup()
+                           .addGroup(GroupLayout.Alignment.LEADING, dlgAcercaDeLayout.createSequentialGroup()
+                               .addComponent(btnCerrar, GroupLayout.PREFERRED_SIZE, 104, GroupLayout.PREFERRED_SIZE)
+                               .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 0, Short.MAX_VALUE))
+                           .addGroup(dlgAcercaDeLayout.createSequentialGroup()
+                               .addComponent(lblAcercaDe2, GroupLayout.PREFERRED_SIZE, 108, GroupLayout.PREFERRED_SIZE)
+                               .addGap(0, 0, Short.MAX_VALUE)))
+                       .addGap(132)))
+               .addContainerGap(212, 212));
+            dlgAcercaDeLayout.setVerticalGroup(dlgAcercaDeLayout.createSequentialGroup()
+               .addContainerGap()
+               .addComponent(lblAcercaDe1, GroupLayout.PREFERRED_SIZE, 15, GroupLayout.PREFERRED_SIZE)
+               .addGap(22)
+               .addComponent(lblAcercaDe2, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
+               .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+               .addComponent(lblAcercaDe3, GroupLayout.PREFERRED_SIZE, 58, GroupLayout.PREFERRED_SIZE)
+               .addGap(20)
+               .addComponent(lblAcercaDe4, GroupLayout.PREFERRED_SIZE, 48, GroupLayout.PREFERRED_SIZE)
+               .addGap(24)
+               .addComponent(btnCerrar, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
+               .addContainerGap(670, Short.MAX_VALUE));
          }
          {
             jMenuBar1 = new JMenuBar();
@@ -88,27 +180,22 @@ public class VentanaPrincipal extends javax.swing.JFrame
                jMenuBar1.add(mnuEjemplos);
                mnuEjemplos.setText("Ejemplos");
                {
-               	mniConsultas = new JMenuItem();
-               	mniConsultas.setText("Consultas  SQL");
-               	mnuEjemplos.add(mniConsultas);
-               	mniConsultas.addActionListener(new ActionListener() {
+                  mniBarcos = new JMenuItem();
+                  mnuEjemplos.add(mniBarcos);
+                  mniBarcos.setText("Dispositivo del inspector");
+                  mniBarcos.addActionListener(new ActionListener() {
                      public void actionPerformed(ActionEvent evt) {
-                        mniConsultasActionPerformed(evt);
+                        mniBarcosActionPerformed(evt);
                      }
                   });
                }
                {
-               	jSeparator1_1 = new JSeparator();
-               	mnuEjemplos.add(jSeparator1_1);
-               }
-              
-               {
-                  mniInspector = new JMenuItem();
-                  mnuEjemplos.add(mniInspector);
-                  mniInspector.setText("Unidad personal del inspector");
-                  mniInspector.addActionListener(new ActionListener() {
+                  mniConsultas = new JMenuItem();
+                  mnuEjemplos.add(mniConsultas);
+                  mniConsultas.setText("Consultas");
+                  mniConsultas.addActionListener(new ActionListener() {
                      public void actionPerformed(ActionEvent evt) {
-                    	 mniInspectorActionPerformed(evt);
+                        mniConsultasActionPerformed(evt);
                      }
                   });
                }
@@ -140,15 +227,15 @@ public class VentanaPrincipal extends javax.swing.JFrame
    {
       this.dispose();
    }
-  
-   private void mniInspectorActionPerformed(ActionEvent evt) 
+   
+   private void mniBarcosActionPerformed(ActionEvent evt) 
    {
       try
       {
-         this.ventanaInspector.setMaximum(true);
+         this.ventanaBarcos.setMaximum(true);
       }
       catch (PropertyVetoException e) {}
-      this.ventanaInspector.setVisible(true);      
+      this.ventanaBarcos.setVisible(true);      
    }
    
    private void mniConsultasActionPerformed(ActionEvent evt) 
@@ -159,6 +246,11 @@ public class VentanaPrincipal extends javax.swing.JFrame
       }
       catch (PropertyVetoException e) {}
       this.ventanaConsultas.setVisible(true);      
+   }
+
+   private void btnCerrarActionPerformed(ActionEvent evt) 
+   {
+      dlgAcercaDe.setVisible(false);
    }
    
    private void centrarDialogo(javax.swing.JDialog p_dialogo)
