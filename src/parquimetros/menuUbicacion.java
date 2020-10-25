2b0fdff356e1891388c6601c917c479c5d9910e6
IVChristos <anonymousvsworld@users.noreply.github.com>
2020-10-25 13:04:26 -0300

    Update menuUbicacion.java
    creado menu Ubicacion

--- src/parquimetros/menuUbicacion.java
+++ src/parquimetros/menuUbicacion.java
@@ -1,5 +1,23 @@
 package parquimetros;
+import javax.swing.JMenuBar;
 
-public class menuUbicacion {
+import quick.dbtable.DBTable;
+
+public class menuUbicacion extends javax.swing.JDialog{
+	
+	private DBTable tabla;
+	
+	public menuUbicacion(DBTable tabla) {
+		setModal(true);
+		initialize();
+		this.tabla = tabla;
+	}
+	private void initialize() {
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+	}
+	
+	
 
 }
